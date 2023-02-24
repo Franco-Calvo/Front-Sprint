2ad4ac24c7dd84c6fe0d51e9cd87dd542a5f278b
Franco Calvo <106486606+Franco-Calvo@users.noreply.github.com>
2023-02-24 16:13:53 -0300

    M 01 (#1)
    * Hero01Ready
    
    * TerminadaLaSecciónHero1

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import HeroMain from "./Components/HeroMain/HeroMain";
+
+export default function App() {
+  return (
+    <>
+      <HeroMain/>
+    </>
+  );
+}
