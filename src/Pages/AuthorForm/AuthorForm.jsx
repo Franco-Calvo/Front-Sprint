9bc8ff8cdb661641e6d881f76ef676a97c87d9d1
DanielOQ <danielorozco0208@gmail.com>
2023-03-03 16:14:02 -0500

    add author form page

---
+++ src/Pages/AuthorForm/AuthorForm.jsx
@@ -0,0 +1,100 @@
+import {useRef} from "react";
+import "./AuthorForm.css";
+import axios from "axios";
+import swal from "sweetalert";
+
+export default function AuthorForm() {
+  const firstName = useRef();
+  const lastName = useRef();
+  const cityCountry = useRef();
+  const date = useRef();
+  const urlProfile = useRef();
+  const formRef = useRef();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    let token = localStorage.getItem("token");
+    let headers = {headers: {Authorization: `Bearer ${token}`}};
+    if (cityCountry.current.value.includes(",")) {
+      let city = cityCountry.current.value.split(",")[0].trim();
+      let country = cityCountry.current.value.split(",")[1].trim();
+      let data = {
+        [firstName.current.name]: firstName.current.value,
+        [lastName.current.name]: lastName.current.value,
+        ["city"]: city,
+        ["country"]: country,
+        [date.current.name]: date.current.value,
+        [urlProfile.current.name]: urlProfile.current.value,
+        ["user_id"]: "63fe72b7c75248e38d293139",
+        ["active"]: true,
+      };
+      let url = "http://localhost:8080/authors";
+      try {
+        await axios.post(url, data, headers);
+        formRef.current.reset();
+        swal({
+          title: "Great!",
+          text: "Author created successfully.",
+          icon: "success",
+          button: "Accept",
+          timer: "10000",
+        });
+      } catch (error) {
+        console.log(error);
+        swal({
+          title: "Erro",
+          text: error.response.data.message[0],
+          icon: "error",
+          button: "Go Back",
+          timer: "10000",
+        });
+      }
+    } else {
+      swal({
+        title: "Upss!",
+        text: "The city and the country must be separated by a comma.",
+        icon: "warning",
+        button: "Go Back",
+        timer: "10000",
+      });
+    }
+  }
+  return (
+    <div id="container-authorform">
+      <form ref={formRef} id="author-form" onSubmit={handleSubmit}>
+        <h1>New Author</h1>
+        <img src="./default-profile.png" alt="profile" />
+        <input
+          ref={firstName}
+          name="name"
+          className="inputAuthorForm"
+          type="text"
+          placeholder="First Name"
+        />
+        <input
+          ref={lastName}
+          name="last_name"
+          className="inputAuthorForm"
+          type="text"
+          placeholder="Last Name"
+        />
+        <input
+          ref={cityCountry}
+          name="city_country"
+          className="inputAuthorForm"
+          type="text"
+          placeholder="City, Country"
+        />
+        <input name="date" ref={date} className="inputAuthorForm" type="date" />
+        <input
+          ref={urlProfile}
+          name="photo"
+          className="inputAuthorForm"
+          type="text"
+          placeholder="URL Profile Image"
+        />
+        <input id="button-author" type="submit" value="Send" />
+      </form>
+    </div>
+  );
+}
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,22 +1,41 @@
 import NotFound from "./NotFound/NotFound";
-import Index from "./Index/Index"
-import IndexLayout from "../Layouts/IndexLayout/IndexLayout"
+import Index from "./Index/Index";
+import IndexLayout from "../Layouts/IndexLayout/IndexLayout";
+import MainLayout from "../Layouts/MainLayout/MainLayout";
 
-import { createBrowserRouter } from "react-router-dom";
+import {createBrowserRouter} from "react-router-dom";
+import AuthorForm from "./AuthorForm/AuthorForm";
 
 // @createBrowserRouter recibe un array de objetos, cada objeto tiene dos propiedades @Path la ruta @Element el elemento (componente de página que se renderizará en esa ruta)
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <IndexLayout/>,
-    children: [{
-      path: "/",
-      element: <Index/>
-    },{
-      path: "/*",
-      element: <NotFound/>
-    }]
+    element: <IndexLayout />,
+    children: [
+      {
+        index: true,
+        element: <Index />,
+      },
+      {
+        path: "/*",
+        element: <NotFound />,
+      },
+    ],
   },
-  {path: "/notfound", element: <NotFound/>}
-])
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/author-form",
+        element: <AuthorForm />,
+      },
+      {
+        path: "/*",
+        element: <NotFound />,
+      },
+    ],
+  },
+  {path: "/notfound", element: <NotFound />},
+]);
