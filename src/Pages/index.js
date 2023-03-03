18119c8f68c0873394278b41dff9c6b303fe00da
DanielOQ0 <93298398+DanielOQ0@users.noreply.github.com>
2023-03-03 16:53:15 -0500

    M 06 (#7)
    * M 04 (#4)
    
    * Hero01Ready
    
    * Imágenes
    
    * Creación del Index terminado
    
    * MainLayout/Footer/Index
    
    * Cambios
    
    * add author form page
    
    ---------
    
    Co-authored-by: Franco Calvo <106486606+Franco-Calvo@users.noreply.github.com>
    Co-authored-by: DanielOQ <danielorozco0208@gmail.com>

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -2,29 +2,47 @@ import NotFound from "./NotFound/NotFound";
 import Index from "./Index/Index";
 import IndexLayout from "../Layouts/IndexLayout/IndexLayout";
 import MainLayout from "../Layouts/MainLayout/MainLayout";
-import { createBrowserRouter } from "react-router-dom";
-import HeroMain from "./HeroMain/HeroMain";
+import {createBrowserRouter} from "react-router-dom";
 import HeroRegister from "./HeroRegister/HeroRegister";
 import Auth from "./Auth/Auth";
+import AuthorForm from "./AuthorForm/AuthorForm";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <IndexLayout />,
     children: [
-      { path: "/", element: <Index /> },
-      { path: "/hero", element: <HeroMain /> },
       {
-        path: "/signup",
-        element: <MainLayout />,
-        children: [{ path: "/signup", element: <HeroRegister /> }],
+        index: true,
+        element: <Index />,
       },
       {
-        path: "/signin",
-        element: <MainLayout />,
-        children: [{ path: "/signin", element: <Auth /> }],
+        path: "/*",
+        element: <NotFound />,
       },
     ],
   },
-  { path: "/*", element: <NotFound /> },
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/author-form",
+        element: <AuthorForm />,
+      },
+      {
+        path: "/signup",
+        element: <HeroRegister />,
+      },
+      {
+        path: "/signin",
+        element: <Auth />,
+      },
+      {
+        path: "/*",
+        element: <NotFound />,
+      },
+    ],
+  },
+  {path: "/notfound", element: <NotFound />},
 ]);
