1d7d28e90ebbe15e118b4405d14b93c2f6de4ab5
DanielOQ <danielorozco0208@gmail.com>
2023-03-06 21:12:30 -0500

    correciones css

--- src/Pages/ChapterForm/ChapterForm.jsx
+++ src/Pages/ChapterForm/ChapterForm.jsx
@@ -1,16 +1,14 @@
-import React from 'react'
-import FormChapter from '../../Components/FormChapter/FormChapter'
-import Header from '../../Components/Header/Header'
-import './chapterform.css'
+import React from "react";
+import FormChapter from "../../Components/FormChapter/FormChapter";
+import Header from "../../Components/Header/Header";
+import "./chapterform.css";
 export default function HeroChapter() {
   return (
     <>
-    <Header/>
-    <div className='newchapter'>
+      <div className="newchapter">
         <h2>New Chapter</h2>
-        <FormChapter/>
-    </div>
-   
+        <FormChapter />
+      </div>
     </>
-  )
+  );
 }
