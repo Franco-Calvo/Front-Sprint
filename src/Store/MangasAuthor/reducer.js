d449b55546b8673087e4ed9b17b51a9bd912ac2a
DanielOQ <danielorozco0208@gmail.com>
2023-03-13 13:52:58 -0500

    add store in manga author

--- src/Store/MangasAuthor/reducer.js
+++ src/Store/MangasAuthor/reducer.js
@@ -1,13 +1,14 @@
 import {createReducer} from "@reduxjs/toolkit";
 import mangasActions from "./actions";
 const {read_mangas} = mangasActions;
-const initialState = {mangas: []};
+const initialState = {new: [], old: []};
 
 const mangasReducer = createReducer(initialState, (builder) => {
   builder.addCase(read_mangas.fulfilled, (state, action) => {
     let newState = {
       ...state,
-      mangas: action.payload.mangas,
+      new: action.payload.new,
+      old: action.payload.old,
     };
     return newState;
   });
