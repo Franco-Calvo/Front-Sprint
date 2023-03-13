12d05165b676e09f916f042f6813b27d66934004
Franco Calvo <fcalvo33@gmail.com>
2023-03-13 17:15:58 -0300

    Fix

---
+++ src/Store/store.js
@@ -0,0 +1,20 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authorReducer from "./Author/reducer";
+import checkReducer from "./CheckAuthor/reducer";
+import mangasReducer from "./MangasAuthor/reducer";
+import alertReducer from "./Alert/reducer";
+import textReducer from "./Text/reducer";
+import eventReducer from "./Events/reducer";
+import checksReducer from "./Checks/reducer";
+
+export const store = configureStore({
+  reducer: {
+    alert: alertReducer,
+    checkboxAuthor: checkReducer,
+    Author: authorReducer,
+    MangasAuthor: mangasReducer,
+    text: textReducer,
+    events: eventReducer,
+    checks: checksReducer,
+  },
+});
