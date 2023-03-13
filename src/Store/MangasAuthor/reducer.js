f7d72f7bee928e71c9948fbf069f95da2dcadc7d
DanielOQ <danielorozco0208@gmail.com>
2023-03-13 09:36:19 -0500

    fix

---
+++ src/Store/MangasAuthor/reducer.js
@@ -0,0 +1,15 @@
+import {createReducer} from "@reduxjs/toolkit";
+import mangasActions from "./actions";
+const {read_mangas} = mangasActions;
+const initialState = {mangas: []};
+
+const mangasReducer = createReducer(initialState, (builder) => {
+  builder.addCase(read_mangas.fulfilled, (state, action) => {
+    let newState = {
+      ...state,
+      mangas: action.payload.mangas,
+    };
+    return newState;
+  });
+});
+export default mangasReducer;
