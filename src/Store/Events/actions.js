ef6d3a3318bfed5732d9b981b586ece02fbea95f
Franco Calvo <106486606+Franco-Calvo@users.noreply.github.com>
2023-03-13 17:20:05 -0300

    M10 front (#25)
    * Auth Falta Responsive
    
    * Cambios en el front, falta todavía
    
    * Cambios en el index, faltan cositas y detalles
    
    * cambios en el index
    
    * Responsive, detalles estéticos en links
    
    * Solución de errores, se agregó alerta al iniciar sesión
    
    * .
    
    * Arreglos antes de iniciar con el tercer sprint
    
    * Cambios en el front
    
    * Fix store
    
    * .
    
    * Front casi terminado, falta responsive
    
    * Fix

--- src/Store/Events/actions.js
+++ src/Store/Events/actions.js
@@ -1,10 +1,10 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const read_events = createAsyncThunk("read_events", async ({ inputText }) => {
+const read_events = createAsyncThunk("read_events", async ({ inputText, captureChecks, pages }) => {
   try {
     let response = await axios.get(
-      "http://localhost:8080/mangas/view?title="+inputText.trim()
+      "http://localhost:8080/mangas/view?title="+inputText.trim()+"&category="+captureChecks+"&page="+pages,
     );
     return {
       events: response.data.mangas
