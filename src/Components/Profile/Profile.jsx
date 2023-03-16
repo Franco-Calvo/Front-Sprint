ba35ddf284129d5b9c39456ef223d6e5a7dae780
RominaGaite <gaiteromina@gmaul.com>
2023-03-16 19:16:45 -0300

    -

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -3,7 +3,6 @@ import './profile.css'
 
 export default function Profile() {
     return (
-
         <div className='profile'>
             <img id="profile-img" src="./default-profile.png" alt="profile" />
             <h2>romy</h2>
