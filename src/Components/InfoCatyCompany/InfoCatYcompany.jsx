e18065191bc70e799cf4d30320b6e9753ca7795e
RominaGaite <gaiteromina@gmaul.com>
2023-03-20 10:46:30 -0300

    correcciones

--- src/Components/InfoCatyCompany/InfoCatYcompany.jsx
+++ src/Components/InfoCatyCompany/InfoCatYcompany.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import './info.css'
 import { Link as Anchor } from 'react-router-dom'
 
+
 export default function InfoCatYcompany({info}) {
   return (
-      <div className="category-company">
-        <button className="category">{info.category_id.name}</button>
+    <div className="category-company">
+      {info.category_id && <button className="category">{info.category_id.name}</button>}
+      {info.author_id && (
         <Anchor to={"/author/"+ info.author_id._id}>
-        <h3 className="company">{info.author_id.name}</h3>
+          <h3 className="company">{info.author_id.name}</h3>
         </Anchor>
-      </div>
+      )}
+    </div>
   );
 }
